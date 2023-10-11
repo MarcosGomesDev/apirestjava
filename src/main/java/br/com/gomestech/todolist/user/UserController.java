0f4f82c587a709757f0448c679f40e89607c1284
Marcos Gomes <marcosgomesdev@gmail.com>
2023-10-11 08:43:04 -0300

    fix: type response entity

--- src/main/java/br/com/gomestech/todolist/user/UserController.java
+++ src/main/java/br/com/gomestech/todolist/user/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public ResponseEntity create(@RequestBody UserModel userModel) {
+  public ResponseEntity<?> create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
