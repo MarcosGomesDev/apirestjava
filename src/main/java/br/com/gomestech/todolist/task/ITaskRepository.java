495f056f9d07a5e331f4317a647253217de8de19
Marcos Gomes <marcosgomesdev@gmail.com>
2023-10-13 18:58:31 -0300

    feat: create repository task

---
+++ src/main/java/br/com/gomestech/todolist/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package br.com.gomestech.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+  List<TaskModel> findByUserId(UUID userId);
+}
