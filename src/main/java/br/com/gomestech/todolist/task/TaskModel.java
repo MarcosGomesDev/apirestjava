e700274203ed000b5a14af318790c8c772ce32bb
Marcos Gomes <marcosgomesdev@gmail.com>
2023-10-13 18:58:53 -0300

    feat: create task model

---
+++ src/main/java/br/com/gomestech/todolist/task/TaskModel.java
@@ -0,0 +1,41 @@
+package br.com.gomestech.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tasks")
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(length = 50)
+  private String title;
+  private String description;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
+
+  private UUID userId;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("Title must be less than 50 characters");
+    }
+    this.title = title;
+  }
+
+}
